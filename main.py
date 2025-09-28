4290c1f056921e4779f80803e51e1b3f60d92d60
syml1nked <rmoran38@gmail.com>
2025-09-28 12:58:13 -0700

    base model integration

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from llm-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    resp_content = client.models.generate_content(model=model, contents=contents)
+
+    print(resp_content.text)
+    print(f"Prompt tokens: {resp_content.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {resp_content.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
